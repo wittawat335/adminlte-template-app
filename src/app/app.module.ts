800081ee0a2e0fee6ebb0a3635551b54c46565ec
Wittawat Fuangfu <wittawat335@gmail.com>
2023-12-01 16:19:55 +0700

    Add project files.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PagesComponent } from './pages/pages.component';
+import { SharedComponent } from './shared/shared.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent,
+    PagesComponent,
+    SharedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
